108d8ab6830361f5bee284be76fe652057fa7334
Nick Khomiakov <khomyakov.nikita@gmail.com>
2023-11-04 11:56:04 +0000

    Changed bunch of stuff, bugfix

--- index.py
+++ index.py
@@ -50,26 +50,26 @@ pen.speed(0)
 pen.penup()
 pen.hideturtle()
 pen.color('#FAFAFA')
-pen.goto(0, 590)
+pen.goto(0, 250)
 pen.write('Score: 0 │ Highscore: 0', align='center', font='Inter 24 bold')
 
 # Snake Movement
 
 def snake_up():
     if snake_head.direction != 'down':
-        snake_head.direction == 'up'
+        snake_head.direction = 'up'
 
 def snake_down():
     if snake_head.direction != 'up':
-        snake_head.direction == 'down'
+        snake_head.direction = 'down'
 
 def snake_left():
     if snake_head.direction != 'left':
-        snake_head.direction == 'right'
+        snake_head.direction = 'right'
 
 def snake_right():
     if snake_head.direction != 'right':
-        snake_head.direction == 'left'
+        snake_head.direction = 'left'
 
 def snake_move():
     if snake_head.direction == 'up':
@@ -93,8 +93,8 @@ def snake_move():
 client_window.listen()
 client_window.onkeypress(snake_up, 'w')
 client_window.onkeypress(snake_down, 's')
-client_window.onkeypress(snake_left, 'a')
-client_window.onkeypress(snake_right, 'd')
+client_window.onkeypress(snake_left, 'd')
+client_window.onkeypress(snake_right, 'a')
 
 # Snake Segments
 
@@ -114,7 +114,7 @@ while True:
         apple_food.color(apple_colours)
 
         for i in snake_segments:
-            snake_segments.goto(5000, 5000)
+            i.goto(5000, 5000)
 
         snake_segments.clear()
         user_score = 0
@@ -125,7 +125,7 @@ while True:
                   font='Inter 24 bold')
 
     if snake_head.distance(apple_food) < 20:
-        apple_food.goto(randint(-600, 600), randint(-350, 350))
+        apple_food.goto(randrange(-600, 600, 20), randrange(-360, 360, 20))
 
         new_segment = Turtle()
         new_segment.speed(0)
@@ -145,7 +145,7 @@ while True:
         pen.clear()
 
         pen.write(f'Score: {user_score} │ Highscore: {high_user_score}', align='center',
-                  font='Inter, 24, bold')
+                  font='Inter 24 bold')
 
     for j in range(len(snake_segments) - 1, 0, -1):
         x = snake_segments[j-1].xcor()
@@ -164,12 +164,12 @@ while True:
             sleep(1)
             snake_head.home()
 
-            snake_head.direction('stop')
+            snake_head.direction = 'stop'
             apple_colours = choice(['#febf20', '#f25a0e', '#fe1e1a', '#b81e46', '#1c50d6'])
             apple_shapes = choice(['triangle', 'square', 'circle'])
 
             for i in snake_segments:
-                snake_segments.goto(5000, 5000)
+                i.goto(5000, 5000)
 
             snake_segments.clear()
             user_score = 0
